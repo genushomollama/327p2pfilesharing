2b9904138bb61f00ceb5c1c939ddb7142cc96dc3
genushomollama <gregorylitmannavarro@gmail.com>
2021-04-27 21:37:08 -0700

    Cleaned up clientThread

--- ClientThread.py
+++ ClientThread.py
@@ -5,8 +5,6 @@ import os
 import time
 import logging
 
-from Node import Node
-
 
 class ClientThread(threading.Thread):
 
@@ -30,7 +28,7 @@ class ClientThread(threading.Thread):
     Preferably check beforehand that the directory exists before calling this function.
     '''
     def makeDirs(self, dir_path):
-        print("Entering mkdirs")
+        logging.debug("Entering makeDirs")
         dirs = dir_path.split('/')
         current_path = ""
         if len(dirs) > 0:
@@ -42,7 +40,7 @@ class ClientThread(threading.Thread):
             current_dir_exists = os.path.exists(current_path)
             if not current_dir_exists:
                 os.mkdir(current_path)
-        print("Leaving mkdirs")
+        logging.debug("Leaving makeDirs")
 
     '''
     Accept another node's manifest as plaintext, parse it into a new dict() structure.
@@ -101,40 +99,38 @@ class ClientThread(threading.Thread):
     This list will be returned to the caller.
     '''
     def checkManifest(self, recv_manifest):
-        print("Entering checkManifest() this is what we got", recv_manifest)
+        logging.debug("Entering checkManifest() this is what we got {}".format(recv_manifest))
         files = list()
         manifest = self.current_manifest.getManifest()
         # iterate through keys
-        for dir in recv_manifest.keys():
-            # print("check ", dir)
+        for directory in recv_manifest.keys():
             files_in_dir = list()
-            # print(files_in_dir)
-            dir_exists = os.path.isdir(dir)# does dir exist
-            dir_in_manifest = False # is the dir in the manifest already?
-            if dir in manifest.keys():
+            dir_exists = os.path.isdir(directory)# does directory exist
+            dir_in_manifest = False # is the directory in the manifest already?
+            if directory in manifest.keys():
                 dir_in_manifest = True
             if not dir_exists: # NO
-                files.append((dir, None)) # time to make records, make one for every item in dirs list, if none only make a arecord for that dir (dirname, None)
+                # time to make records, make one for every item in dirs list,
+                # if none only make a a record for that directory (dirname, None)
+                files.append((directory, None))
             elif dir_in_manifest: # YES
-                files_in_dir = manifest[dir] # get contents of dir in our manifest
+                files_in_dir = manifest[directory] # get contents of directory in our manifest
             else:
                 files_in_dir = list()
-            for item in recv_manifest[dir]: # iterate through the items in recv_manifest[dir]
-                # print(item)
+            for item in recv_manifest[directory]: # iterate through the items in recv_manifest[dir]
                 if item not in files_in_dir:
-                    # print("item was not in filedir")
-                    files.append((dir, item)) # if any are missing, make a record for each
-        print("Leaving checkManifest, this is what we got", files)
+                    files.append((directory, item)) # if any are missing, make a record for each
+        logging.debug("Leaving checkManifest, this is what we got {}".format(files))
         return files # list of tuples [(dir1, file1), (dir3, file2), ...]
 
     '''
     Download all outstanding files from the neighbor's manifest.
     # TODO implement a ThreadPoolExecutor to issue requests & download files.
-    Returns a list of filepaths (to successfully downloaded files) to parse and record in the manifest.
+    Returns a list of file paths (to successfully downloaded files) to parse and record in the manifest.
     '''
     def download_files(self, files):
-        print("Entering download_files(), we will attempt to download the following files {}".format(files))
-        downloaded_files = list()  # records of downloaded files will be returned, list of filepaths
+        logging.debug("Entering download_files(), we will attempt to download the following files {}".format(files))
+        downloaded_files = list()  # records of downloaded files will be returned, list of file paths
         for item in files: # iterate through
             directory = item[0] # location of target directory
             filename = item[1] # filename
@@ -143,9 +139,9 @@ class ClientThread(threading.Thread):
                 intermediate_dirs = directory.split("/")
                 if len(intermediate_dirs) > 1: # if less than one than the only directory is 'Shared', this should not occur
                     hostname = intermediate_dirs[1]
-                     # connect to host and download the file
+                    # connect to host and download the file
                     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as neighbor_conn:
-                        print("We have a opened a connection to the other host to get {}".format(filename))
+                        logging.debug("We have a opened a connection to the other host to get {}".format(filename))
                         neighbor_conn.connect((hostname, self.port))
                         file_location = directory + "/" + filename
                         request = "FILE {}".format(file_location)
@@ -162,25 +158,22 @@ class ClientThread(threading.Thread):
                                     num_recv_calls = 1
                                 else:
                                     num_recv_calls = math.ceil(file_size/self.MAX_DATA_TRANS)
-                                print("We are in the act of downloading the file")
                                 while recv_calls_made < num_recv_calls:
-                                    print("Getting data")
-                                    data_in = neighbor_conn.recv(self.MAX_DATA_TRANS)  # check buffer for more data # FIXME hangup occurs here
+                                    data_in = neighbor_conn.recv(self.MAX_DATA_TRANS)  # check buffer for more data
                                     recv_calls_made += 1
-                                    print("got data")
                                     file_in.write(data_in)  # write the data to the file
-                                print("We have finished downloading the file")
+                                logging.debug("We have finished downloading the file {}".format(filename))
                                 file_in.close()
                                 downloaded_files.append(item)
                             else:
-                                pass  # TODO log error
+                                logging.error("The serving peer can not serve the file {}/{}".format(directory, filename))
                         else:
-                            pass  # TODO log error
+                            logging.error("The request for the file {}/{} was met with an expected response".format(directory, filename))
             else:
                 if not os.path.exists(directory):
-                    self.makeDirs(directory) # TODO test
+                    self.makeDirs(directory)
                     downloaded_files.append(item)
-        print("Leaving download_files()", downloaded_files)
+        logging.debug("Leaving download_files() with the following files {}".format(downloaded_files))
         return downloaded_files
 
     '''
@@ -189,14 +182,14 @@ class ClientThread(threading.Thread):
     to our manifest.
     '''
     def update_manifest(self, downloaded_files):
-        print("Entering update_manifest()", downloaded_files)
+        logging.debug("Entering update_manifest()\nAdding the following files to manifest {}".format(downloaded_files))
         for item in downloaded_files:
             if item[1] is None:
                 self.current_manifest.addDir(item[0], list())
             else:
-                logging.debug("{}".format(item))
+                logging.debug("Adding {}/{} to manifest".format(item[0], item[1]))
                 self.current_manifest.addFileToDir(item[0], item[1])
-        logging.debug('ClientThread: Leaving update_manifest() {}'.format(self.current_manifest.getManifest()))
+        logging.debug('ClientThread: Leaving update_manifest() with the updated manifest {}'.format(self.current_manifest.getManifest()))
 
     '''
     Send messages informing neighbor nodes that we are leaving the overlay.
@@ -205,42 +198,37 @@ class ClientThread(threading.Thread):
     # TODO handle any network problems that could arise
     '''
     def sendLeaveRequest(self, last_addr, next_addr=None):
-        print("Entering sendLeaveRequest")
+        logging.debug("Entering sendLeaveRequest")
         success = False
         messages = []
         if next_addr is not None:
             messages.append("UPDATE LAST {}".format(last_addr))
             messages.append("UPDATE NEXT {}".format(next_addr))
-            addrs = [last_addr, next_addr]
+            addresses = [last_addr, next_addr]
         else:
             messages.append("UPDATE VOID VOID")
-            addrs = [last_addr]
-        print('We have our messages')
-        # if len(messages) != neighbors: # FIXME a problem is occuring here for some reason this branch is being taken
-        #     print("We have {} messages and {} neighbors so we are going to raise an Exception")
-        #     raise Exception  # TODO find a more specific exception type
+            addresses = [last_addr]
 
         for i in range(len(messages)):
-            print("Sending {} message".format(i))
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as neighbor_conn:
                 try:
-                    print("Made connection to {}".format(addrs[i]))
-                    neighbor_conn.connect((addrs[i], self.port))
+                    logging.debug("Sending leave request to {}".format(addresses[i]))
+                    neighbor_conn.connect((addresses[i], self.port))
                     neighbor_conn.send(messages[i].encode('ascii'))
-                    # reply = neighbor_conn.recv(self.MAX_DATA_TRANS).decode()
+                    # reply = neighbor_conn.recv(self.MAX_DATA_TRANS).decode() # FIXME handle fail state
                     # if reply is "SUCCESS":
                     #     success = True
                     # elif reply is "FAILURE":
                     #     success = False
                 except socket.gaierror:
-                    logging.error("socket.gaierror occurred sending leave request to {}".format(addrs[i]))
+                    logging.error("socket.gaierror occurred sending leave request to {}".format(addresses[i]))
                 except socket.timeout:
-                    logging.error("socket.timeout occurred sending leave request to {}".format(addrs[i]))
+                    logging.error("socket.timeout occurred sending leave request to {}".format(addresses[i]))
                 except ConnectionRefusedError:
-                    logging.error("ConnectionRefusedError occurred sending leave request to {}".format(addrs[i]))
+                    logging.error("ConnectionRefusedError occurred sending leave request to {}".format(addresses[i]))
                 except OSError:
-                    logging.error("OSerror occurred sending leave request to {}".format(addrs[i]))
-        print("Finished sending leave requests, we were", success)
+                    logging.error("OSError occurred sending leave request to {}".format(addresses[i]))
+        logging.debug("Finished sending leave requests, we were".format(success))
         return success
 
     '''
@@ -248,20 +236,20 @@ class ClientThread(threading.Thread):
     it connects directly the neighboring nodes to maintain ring structure.
     '''
     def leaveOverlay(self):
-        print("Entering leaveOverlay()")
+        logging.debug("Entering leaveOverlay()")
         success = False
         last_addr = self.node.getLast()
         if last_addr is not None:
             next_addr = self.node.getNext()
             if last_addr == next_addr:
-                print("Sending leave request to {}".format(last_addr))
+                logging.debug("Sending leave request to {}".format(last_addr))
                 success = self.sendLeaveRequest(last_addr)
             else:
-                print("Sending leave request to {} and {}".format(last_addr, next_addr))
+                logging.debug("Sending leave request to {} and {}".format(last_addr, next_addr))
                 success = self.sendLeaveRequest(last_addr, next_addr)
         else:
             success = True
-        print("Leaving leaveOverlay()")
+        logging.debug("Leaving leaveOverlay()")
         return success
 
     '''
@@ -276,22 +264,14 @@ class ClientThread(threading.Thread):
             logging.debug('ClientThread: Starting loop')
             manifest = self.request_manifest()  # request the manifest
             if manifest is not None:
-                logging.debug('ClientThread: Calling checkmanifest')
-                needed_files = self.checkManifest(manifest) # list of tuples containing (hostname, filepath)  # TODO we will end up with a list of files
+                logging.debug('ClientThread: Calling checkManifest')
+                needed_files = self.checkManifest(manifest) # we will end up with a list of files
                 # TODO we will make a call to the dht to locate a place to download each resource
                 logging.debug('ClientThread: calling download_files')
-                print("Sending needed files to the downloads", needed_files)
-                new_files = self.download_files(needed_files) # TODO we will download each needed resource, and put into the proper directory (add directory if necessary)
+                logging.debug("Sending needed files to the downloads", needed_files)
+                # we will download each needed resource, and put into the proper directory (add directory if necessary)
+                new_files = self.download_files(needed_files)
                 logging.debug('ClientThread: calling update_manifest')
                 self.update_manifest(new_files)  # update our manifest to reflect downloaded files
-            time.sleep(self.sleep_time)# TODO we might need to make a call to sleep or something here, maybe use a scheduler
+            time.sleep(self.sleep_time)# we might need to make a call to sleep or something here, maybe use a scheduler
         clean_exit = self.leaveOverlay()  # TODO something with clean_exit, if everything went well should be True
-
-
-if __name__ == "__main__":
-    print("Starting the program")
-    client_thread = ClientThread()
-    print("Initialized the extended thread subclass (or is it a superclass?)")
-    client_thread.start()
-    client_thread.cleanup()
-    client_thread.join()
