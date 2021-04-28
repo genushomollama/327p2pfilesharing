72f897a8e48ea474fa4658958b7a200e8fbc75e0
genushomollama <gregorylitmannavarro@gmail.com>
2021-04-27 21:54:23 -0700

    Cleaned up ServerThread.py

--- ServerThread.py
+++ ServerThread.py
@@ -4,9 +4,6 @@ import os
 import os.path
 import logging
 
-from Node import Node
-
-
 class ServerThread(threading.Thread):
 
     def __init__(self, current_node, server_port, data_trans_size, manifest, name='ServerThread'):
@@ -81,15 +78,15 @@ class ServerThread(threading.Thread):
     If there is already such a list in memory, send that, if not,
     generate a new list.
     Format will be as follows:
-    DirNam1 residentfile1 residentfile2 residentfile3\n
-    DirNam2 residentfile1 residentfile2 residentfile3\n
+    DirNam1 file1 file2 file3\n
+    DirNam2 file1 file10 file20\n
     '''
     def send_manifest(self, recipient_conn):
         manifest = self.current_manifest.getManifest() # retrieve manifest
         message = "" # message we will send to requester
-        for dir in manifest.keys(): # iterate through contents, put then into string form
-            line = dir
-            dir_files = manifest[dir]
+        for directory in manifest.keys(): # iterate through contents, put then into string form
+            line = directory
+            dir_files = manifest[directory]
             line = line + " " + " ".join(dir_files) + "\n"
             message = message + line
         # open connection other host and send the manifest
@@ -99,15 +96,14 @@ class ServerThread(threading.Thread):
         if manifest_size < self.MAX_DATA_TRANS: # if the manifest is not to large then send it
             recipient_conn.send(message.encode('ascii'))
             recipient_conn.send("END".encode('ascii'))
-        print("We have successfully sent the manifest")
-
+        logging.debug("We have successfully sent the manifest")
 
     '''
     Sends a file to a requester.
     TODO raise and handle exceptions
     '''
     def serve_file(self, conn, filepath):
-        print("Serving file {}".format(filepath))
+        logging.debug("Serving file {}".format(filepath))
         if os.path.exists(filepath):  # verify file exists
             file_size = os.path.getsize(filepath)  # get the size of file
             file_out = open(filepath, 'rb')  # open file for reading bytes
@@ -119,44 +115,43 @@ class ServerThread(threading.Thread):
                 sent_bytes += conn.send(sending_block)  # send block
                 sending_block = file_out.read(self.MAX_DATA_TRANS)  # read next block
             file_out.close()  # close file
-        print("Finished serving file {}".format(filepath))
+        logging.debug("Finished serving file {}".format(filepath))
 
     '''
     Handles all incoming connections from other nodes.
     Depending on the request, server() invokes a number of other functions to fulfill it.
     '''
     def server(self, conn, client_ip, network_size=None):
-        print("Entering the server function in ServerThread")
+        logging.debug("Entering the server function in ServerThread")
         data = conn.recv(2048)  # expected max ~ 1500 bytes
-        print("We received the message {}".format(data.decode()))
+        logging.debug("We received the message {}".format(data.decode()))
         if data:
-            print("Entering the decision in ServerThread")
-            reply = None  # response message to client FIXME maybe should be None
+            logging.debug("Entering the decision in ServerThread")
+            reply = None  # response message to client
             msg = data.decode()  # put message into string form
 
             if "JOIN" in msg:
                 logging.debug("We received a JOIN request from {}".format(client_ip))
-                print("Entering the join branch")
                 if self.node.getNodeID() == 0:
                     logging.debug("We are node 0, we are serving the request")
                     self.updatePrevious(self.node.getLast(), client_ip)
 
                     reply = "SUCCESS {} {}".format(self.network_size, self.node.getLast())
                     self.network_size += 1 # increment so that next assignable node id is ready
-                    print("Sending this reply to client:", reply)
+                    logging.debug("Sending this reply to client:", reply)
                     self.node.setLast(client_ip)
                     logging.debug("Our last node is now {}".format(client_ip))
                     if self.node.getNext() is None:
                         self.node.setNext(client_ip)
                         logging.debug("Our next node is now {}".format(client_ip))
                     self.network_size += 1
-                    print("We successfully allowed the new node to join our overlay network")
+                    logging.debug("We successfully allowed the new node to join our overlay network")
                 else:
                     logging.debug("We are not node 0, we aren't serving the request")
-                    reply = "REDIRECT {} -1 -1".format(self.node.getNext())  # redirect supplicant to the next node in the list
+                    # redirect supplicant to the next node in the list
+                    reply = "REDIRECT {} -1 -1".format(self.node.getNext())
                 conn.send(reply.encode('ascii'))
             elif msg.split()[0] == "UPDATE": # the topology has changed, accept new neighbors
-                # TODO check valid message length
                 if msg.split()[1] == "NEXT":
                     self.node.setNext(msg.split()[2])
                 elif msg.split()[1] == "LAST":
@@ -165,26 +160,25 @@ class ServerThread(threading.Thread):
                     self.node.setLast(None)
                     self.node.setNext(None)
                 else:
-                    print("ERROR WE HAVE AN ERROR UPDATING OUR NODE INFORMATION", msg)  # TODO log error here
+                    logging.error("ERROR WE HAVE AN ERROR UPDATING OUR NODE INFORMATION".format(msg))
             elif msg.split()[0] == "DECREMENT":
                 self.handleDecrement() # decrements id (unless 0), tells the NEXT node in chain to DECREMENT
             elif "MANIFEST" in msg:
                 self.send_manifest(conn)
             elif "FILE" in msg:
-                # TODO check valid message length
-                print("calling serve file")
                 self.serve_file(conn, msg.split()[1])
-                print("we have served the file")
             else:
-                print("THE REQUEST RECEIVED IS UNKNOWN:", msg)
-                # TODO implement error handling
-        print("finished serving client, continuing to listen for more connections :)")
+                logging.debug("THE REQUEST RECEIVED IS UNKNOWN:", msg)
+        logging.info("finished serving client, continuing to listen for more connections :)")
 
     '''
     Opens a listening socket and dispatches other threads,
     running the server() function, to handle those connections.
     Stops accepting new connections when cleanup() is called.
-    Afterwards join() can be called to terminate the thread.s
+    Afterwards join() can be called to terminate the thread.
+    
+    Design informed by the python cookbook and 
+    https://stackoverflow.com/questions/16734534/close-listening-socket-in-python-thread
     '''
     def run(self):
         # bind, listen, while connection active accept
@@ -195,10 +189,13 @@ class ServerThread(threading.Thread):
             server_listen.listen()  # start listening for incoming connections
             while not self._stopevent.isSet():
                 try:
-                    conn, addr = server_listen.accept()  # block while waiting for new connections, these will be represented by the conn object
+                    # block while waiting for new connections, these will be represented by the conn object
+                    conn, addr = server_listen.accept()
                     # start new thread to communicate with the client
                     client_connection = threading.Thread(target=self.server, args=(conn, addr[0], self.network_size))
                     client_connection.start()
                 except socket.timeout:
                     pass # loop back and continue listening
-                # TODO maybe handle OSERROR https://stackoverflow.com/questions/16734534/close-listening-socket-in-python-thread
+                except OSError:
+                    pass # loop back and continue listening
+                # TODO maybe handle OSError
